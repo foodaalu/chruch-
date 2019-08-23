6c3c90f435770e1649e859ec224b36faefcacb79
saroj <sarojghising13@gmail.com>
2019-08-23 15:22:18 +0545

    modified

--- js/numscroll.js
+++ js/numscroll.js
@@ -41,11 +41,7 @@
         return this.each(function(i,obj){
             $(this).addClass('scrollzip');
             if ( $.isFunction( settings.showFunction ) ){
-                if(
-                    !$(this).hasClass('isShown')&&
-                    ($(window).outerHeight()+$('#scrollzipPoint').offset().top-settings.showShift)>($(this).offset().top+((settings.wholeVisible)?$(this).outerHeight():0))&&
-                    ($('#scrollzipPoint').offset().top+((settings.wholeVisible)?$(this).outerHeight():0))<($(this).outerHeight()+$(this).offset().top-settings.showShift)
-                ){
+                {
                     $(this).addClass('isShown');
                     settings.showFunction.call( this );
                 }
